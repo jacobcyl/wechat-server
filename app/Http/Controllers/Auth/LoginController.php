2a03f08a87374baf9a65e923a2bedbd67911d1c9
jacobcyl <cyl.jacob@gmail.com>
2016-11-18 00:23:16 +0800

    initial larval 5.3 framwork

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -35,5 +36,20 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest', ['except' => 'logout']);
+    }
+
+    /**
+     * Handle an authentication attempt.
+     *
+     * @return Response
+     */
+    public function authenticate(Request $request)
+    {
+        $email = $request->get('email');
+        $password = $request->get('password');
+        if (Auth::attempt(['email' => $email, 'password' => $password])) {
+            // Authentication passed...
+            return redirect()->intended('dashboard');
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,5 +14,6 @@ use Illuminate\Http\Request;
 */
 
 Route::get('/user', function (Request $request) {
+    return 'hello world';
     return $request->user();
 })->middleware('auth:api');
